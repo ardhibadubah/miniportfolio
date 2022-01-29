93e686817e0ae95c2be2d97eb004fc482bb58cd4
Dadi Nur Ardhi <artdee.dna@gmail.com>
2022-01-30 00:46:58 +0800

    feat: fix package.json

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -2,7 +2,7 @@ import { ModalContact } from '..';
 
 const ContactUs = () => {
   return (
-    <div className='d-flex flex-column align-items-center justify-content-center vh-100 vw-100'>
+    <div className='d-flex flex-column align-items-center justify-content-center bg-light vh-100 vw-100'>
       <h1 className='text-center mb-5'>Interested in working with us?</h1>
       <ModalContact />
     </div>
