9f6c306f6f2e5344d515e90b69a5ee0d7de66227
Dadi Nur Ardhi <artdee.dna@gmail.com>
2022-01-30 17:24:32 +0800

    feat: integrated API for message form

--- src/components/ContactUs/ModalContact.jsx
+++ src/components/ContactUs/ModalContact.jsx
@@ -1,11 +1,46 @@
+import axios from 'axios';
 import { useState } from 'react';
 import { Button, FloatingLabel, Form, Modal } from 'react-bootstrap';
+import { toast, ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const ModalContact = () => {
   const [show, setShow] = useState(false);
 
+  const [talk, setTalk] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_BASE_URL}/talk`, talk)
+      .then((res) => {
+        alert('pesan berhasil terkirim');
+        setShow(false);
+        toast.success('Pesan berhasil terkirim!', {
+          position: 'bottom-left',
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      })
+      .catch((error) => {
+        toast.error(error.message, {
+          className: 'Toastify__toast-theme--colored ',
+        });
+      });
+  };
 
   return (
     <>
@@ -19,25 +54,50 @@ const ModalContact = () => {
         </Modal.Header>
         <Modal.Body>
           <FloatingLabel controlId='floatingName' label='Name' className='mb-3'>
-            <Form.Control type='text' placeholder='Name' />
+            <Form.Control
+              type='text'
+              placeholder='Name'
+              value={talk.name}
+              onChange={(e) => {
+                setTalk({ ...talk, name: e.target.value });
+              }}
+            />
           </FloatingLabel>
           <FloatingLabel
             controlId='floatingEmail'
             label='Email'
             className='mb-3'>
-            <Form.Control type='email' placeholder='Email' />
+            <Form.Control
+              type='email'
+              placeholder='Email'
+              value={talk.email}
+              onChange={(e) => {
+                setTalk({ ...talk, email: e.target.value });
+              }}
+            />
           </FloatingLabel>
           <FloatingLabel
             controlId='floatingSubject'
             label='Subject'
             className='mb-3'>
-            <Form.Control type='text' placeholder='Subject' />
+            <Form.Control
+              type='text'
+              placeholder='Subject'
+              value={talk.subject}
+              onChange={(e) => {
+                setTalk({ ...talk, subject: e.target.value });
+              }}
+            />
           </FloatingLabel>
           <FloatingLabel controlId='floatingMessage' label='Message'>
             <Form.Control
               as='textarea'
               placeholder='Leave a message here'
               style={{ height: '100px' }}
+              value={talk.message}
+              onChange={(e) => {
+                setTalk({ ...talk, message: e.target.value });
+              }}
             />
           </FloatingLabel>
         </Modal.Body>
@@ -45,10 +105,21 @@ const ModalContact = () => {
           <Button variant='secondary' onClick={handleClose}>
             Cancel
           </Button>
-          <Button type='submit' variant='dark' onClick={handleClose}>
+          <Button type='submit' variant='dark' onClick={handleSubmit}>
             Send
           </Button>
         </Modal.Footer>
+        <ToastContainer
+          position='bottom-left'
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </Modal>
     </>
   );
--- src/components/Navbar/NavigationBar.jsx
+++ src/components/Navbar/NavigationBar.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { ModalContact } from '..';
 
 const NavigationBar = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <Navbar bg='light' fixed='top'>
-      <Container>
-        <Navbar.Brand href='/' className='h1'>
-          <strong>Dev.</strong>
-        </Navbar.Brand>
-        <Nav className='ms-auto'>
-          <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/'>Contact Us</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar bg='light' fixed='top'>
+        <Container>
+          <Navbar.Brand href='/' className='h1'>
+            <strong>Dev.</strong>
+          </Navbar.Brand>
+          <Nav className='ms-auto'>
+            <Nav.Link href='/'>Home</Nav.Link>
+            <Nav.Link href='/'>Contact Us</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
